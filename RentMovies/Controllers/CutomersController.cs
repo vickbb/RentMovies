383f15f7ad4978cbbc214e3b5135e7d1d310d3d0
vickbb <victor_barreiros@hotmail.com>
2019-09-03 00:07:44 -0300

    Add project files.

---
+++ RentMovies/Controllers/CutomersController.cs
@@ -0,0 +1,39 @@
+﻿using RentMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentMovies.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private MyDBContext _context;
+
+        public CustomersController()
+        {
+            _context = new MyDBContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(_ => _.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ RentMovies/Models/MyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using RentMovies.Models;
+using System;
+using System.Data.Entity;
+
+namespace RentMovies.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        internal object SingleOrDefault(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
